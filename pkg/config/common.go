a9fe695298c160902cffbcf726f95427617db2cc
Mario <salem8@gmail.com>
2020-05-28 11:57:32 +0300

    Split config interface

--- pkg/config/common.go
+++ pkg/config/common.go
@@ -1,38 +1,8 @@
 package config
 
-type tasksBootstrapConfiguration struct {
-	kv map[string]interface{}
-}
-
 type TaskConfiguration interface {
 	Add(key string, value interface{})
 	Get(key string) (interface{}, bool)
 	// GetRPC() grpc.TaskCommunicator_RunWorkflowClient
 	// SetRPC(grpc.TaskCommunicator_RunWorkflowClient)
-}
-
-func NewTaskConfiguration() TaskConfiguration {
-	return &tasksBootstrapConfiguration{
-		kv: make(map[string]interface{}),
-	}
-}
-
-// func (cfg *tasksBootstrapConfiguration) GetRPC() grpc.TaskCommunicator_RunWorkflowClient {
-// 	return cfg.gRPC
-// }
-//
-// func (cfg *tasksBootstrapConfiguration) SetRPC(
-// 	gRPC grpc.TaskCommunicator_RunWorkflowClient) {
-// 	cfg.gRPC = gRPC
-// 	cfg.Add(GRPCKey, gRPC)
-// }
-//
-func (cfg *tasksBootstrapConfiguration) Add(key string, value interface{}) {
-	cfg.kv[key] = value
-}
-
-func (cfg *tasksBootstrapConfiguration) Get(key string) (interface{}, bool) {
-	val, found := cfg.kv[key]
-
-	return val, found
 }
--- pkg/config/tasksBootstrapConfiguration.go
+++ pkg/config/tasksBootstrapConfiguration.go
@@ -4,19 +4,6 @@ type tasksBootstrapConfiguration struct {
 	kv map[string]interface{}
 }
 
-type TaskConfiguration interface {
-	Add(key string, value interface{})
-	Get(key string) (interface{}, bool)
-	// GetRPC() grpc.TaskCommunicator_RunWorkflowClient
-	// SetRPC(grpc.TaskCommunicator_RunWorkflowClient)
-}
-
-func NewTaskConfiguration() TaskConfiguration {
-	return &tasksBootstrapConfiguration{
-		kv: make(map[string]interface{}),
-	}
-}
-
 // func (cfg *tasksBootstrapConfiguration) GetRPC() grpc.TaskCommunicator_RunWorkflowClient {
 // 	return cfg.gRPC
 // }
