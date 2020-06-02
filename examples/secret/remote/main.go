519a478dccdc1106f9d92cde543acfe22f30eda0
Mario Karagiorgas <salem8@gmail.com>
2020-06-02 18:46:00 +0300

    Register Gx to remote main

--- examples/secret/remote/main.go
+++ examples/secret/remote/main.go
@@ -8,14 +8,16 @@ import (
 	"github.com/tradeline-tech/workflow/types"
 )
 
+const WorkflowNameKey = "secret"
+
 func main() {
 	serverAddress := "127.0.0.1"
 	port := 8999
-	err := client.StartWorkflow(serverAddress, port, types.TaskRunners{
+	err := remote.StartWorkflow(serverAddress, port, types.TaskRunners{
 		/*
 		 * Set Remote Tasks here
 		 */
-		tasks.NewLaunchWorkflow,
+		NewGenGx,
 	})
 	if err != nil {
 		fmt.Printf("server @ %s:%d with error : %s\n", serverAddress, port, err)
